a970333551f15514f0f5e9bd855cc42ca48b275b
ryusu12 <rsy7567@gmail.com>
2025-06-10 11:31:16 +0900

    레벨 4: Interceptor를 활용한 API 로깅
    어드민 사용자만 접근할 수 있는 특정 API에는 접근할 때마다 접근 로그를 기록한다.
    - 인증 실패시, FORBIDDEN 문구와 함께 요청 시각과 URL을 로깅
    - 인증 성공시, 요청 시각과 URL을 로깅
    
    접근 유저의 userRole이 admin인지 확인한다.
    - 토큰의 userRole로 확인하면, 권한을 user로 변경해도 토큰의 userRole 정보는 admin이라서 접근이 가능한 문제 발생
    - DB의 user를 검색하여 userRole이 admin인지 확인
    - admin이 아니면 FORBIDDEN 예외처리 반환

---
+++ src/main/java/org/example/expert/config/AdminUserInterceptor.java
@@ -0,0 +1,45 @@
+package org.example.expert.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.expert.domain.common.exception.InvalidRequestException;
+import org.example.expert.domain.user.entity.User;
+import org.example.expert.domain.user.enums.UserRole;
+import org.example.expert.domain.user.repository.UserRepository;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@RequiredArgsConstructor
+public class AdminUserInterceptor implements HandlerInterceptor {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull Object handler
+    ) throws Exception {
+        // JwtFilter 에서 set 한 userId 값을 가져옴
+        Long userId = (Long) request.getAttribute("userId");
+
+        // 본인의 권한을 admin 에서 user로 변경하면 여전히 토큰의 userRole이 admin이라서 접근이 가능한 문제 발생
+        // DB의 user를 검색하여 userRole이 admin인지 확인
+        User user = userRepository.findById(userId).orElseThrow(() -> new InvalidRequestException("User not found"));
+
+        if (!UserRole.ADMIN.equals(user.getUserRole())) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "관리자 권한이 없습니다.");
+            log.warn("[FORBIDDEN] url: {}, requestTime: {}", request.getRequestURI(), LocalDateTime.now());
+            return false;
+        }
+
+        log.info("url: {}, requestTime: {}", request.getRequestURI(), LocalDateTime.now());
+        return true;
+    }
+
+}
